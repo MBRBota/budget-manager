96b61bb3ebeb6bba1a993b37f4ee02a354a4ae36
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-04-29 13:26:43 +0300

    feat: implement axios apis

---
+++ src/utils/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const authApi = axios.create({
+  baseURL: "http://localhost:3500",
+  withCredentials: true
+})
+
+export const resourceApi = axios.create({
+  baseURL: "http://localhost:3500"
+})
