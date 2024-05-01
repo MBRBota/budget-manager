50a74e1de7ca18685e2a22d8cf12f90af22f1e62
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-05-01 21:11:01 +0300

    feat: initialize user context and refresh access token on first app render

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import { UserContext } from "./UserContext";
+import { refreshUserToken } from "../services/auth.service";
 
 export default function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const initializeUserContext = async () => {
+        const { data: response } = await refreshUserToken()
+        console.log(response)
+        if (!response?.success) return
+  
+        setUser(response.data)
+      }
+    initializeUserContext()
+  }, [])
+
+  const contextValue = { user, setUser }
   return (
-    <UserContext.Provider>
+    <UserContext.Provider value={contextValue}>
       {children}
     </UserContext.Provider>
   )
