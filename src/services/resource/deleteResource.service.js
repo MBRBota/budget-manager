bb72879e67c1bae866e3356e4543a86609738193
MBRBota <117132555+MBRBota@users.noreply.github.com>
2024-05-23 00:00:44 +0300

    feat: add expense PATCH, DELETE services

--- src/services/resource/deleteResource.service.js
+++ src/services/resource/deleteResource.service.js
@@ -1,6 +1,15 @@
 import { resourceApi } from '../../utils/axios';
 import { APP_CONSTANTS } from '../../utils/constants';
 
+export const deleteExpense = async (accessToken, expenseId) => {
+  const { data } = await resourceApi.delete(APP_CONSTANTS.API_ENDPOINTS.BUDGET_ENDPOINTS.EXPENSE_ENDPOINT, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    data: { expense: expenseId },
+  });
+    
+  return data
+}
+
 export const deleteCategory = async (accessToken, categoryId) => {
   const { data } = await resourceApi.delete(APP_CONSTANTS.API_ENDPOINTS.BUDGET_ENDPOINTS.CATEGORY_ENDPOINT, {
     headers: { Authorization: `Bearer ${accessToken}` },
--- src/services/resource/patchResource.service.js
+++ src/services/resource/patchResource.service.js
@@ -1,6 +1,17 @@
 import { resourceApi } from '../../utils/axios';
 import { APP_CONSTANTS } from '../../utils/constants';
 
+export const patchExpense = async (accessToken, expenseData) => {
+  const { data } = await resourceApi.patch(
+    APP_CONSTANTS.API_ENDPOINTS.BUDGET_ENDPOINTS.EXPENSE_ENDPOINT,
+    // expenseId, expenseSum, expenseDate, categoryId
+    { expense: expenseData },
+    { headers: { Authorization: `Bearer ${accessToken}` } },
+  );
+
+  return data;
+};
+
 export const patchCategory = async (accessToken, categoryData) => {
   const { data } = await resourceApi.patch(
     APP_CONSTANTS.API_ENDPOINTS.BUDGET_ENDPOINTS.CATEGORY_ENDPOINT,
